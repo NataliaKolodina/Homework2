c4cec6637d82a92fc680eb491066cda4a043f9d9
«Natalia <kolodina_melbyk@mail.ru>
2023-01-31 00:04:27 +0400

    add task13

---
+++ task13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// 12457333 -> 4
+
+Console.Clear();
+
+int num = new Random().Next();
+int count = 10;
+int result = (num % count * 10 - num % count) / count;
+if (num > 99)
+{
+    while (num < count * 100)
+    {
+        Console.WriteLine($"{num} -> {result}");
+    }
+    count = count * 10;
+}
+else
+{
+    Console.WriteLine($"{num} -> третьей цифры нет");
+}
